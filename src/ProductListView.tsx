01483a05946e0dc9332ea6083450df14c587fcff
kozlowskipawelwork <kozlowskipawelwork@gmail.com>
2023-03-18 01:48:18 +0100

    added basic trycatch error handling to call

--- src/ProductListView.tsx
+++ src/ProductListView.tsx
@@ -1,18 +1,23 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { Product } from "./types";
 import ProductTable from "./components/ProductTable";
 import AddProductForm from "./components/AddProductForm";
-import "./ProductListView.css"; // Import CSS file
+import "./ProductListView.css";
 
 const ProductList = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [searchQuery, setSearchQuery] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios("https://dummyjson.com/products");
-      setProducts(result.data.products);
+      try {
+        const result = await axios("https://dummyjson.com/products");
+        setProducts(result.data.products);
+      } catch (error) {
+        setError((error as AxiosError).message);
+      }
     };
 
     fetchData();
@@ -33,6 +38,10 @@ const ProductList = () => {
   const filteredProducts = products.filter((product) =>
     product.title.toLowerCase().includes(searchQuery.toLowerCase())
   );
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div>
