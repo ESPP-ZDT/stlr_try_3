f48402d6f7df1798a43a3f8ad4c8329f0a0d4579
kozlowskipawelwork <kozlowskipawelwork@gmail.com>
2023-03-19 02:23:59 +0100

    thumbmail display

--- src/components/ProductTable.tsx
+++ src/components/ProductTable.tsx
@@ -10,36 +10,39 @@ interface Props {
 const ProductTable: React.FC<Props> = ({ products, onDelete }) => {
   return (
     <div>
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Price</th>
-            <th>Rating</th>
-            <th>Stock</th>
-            <th>Brand</th>
-            <th>Category</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          {products.map((product) => (
-            <tr key={product.id}>
-              <td>{product.title}</td>
-              <td>{product.description}</td>
-              <td>{product.price}</td>
-              <td>{product.rating}</td>
-              <td>{product.stock}</td>
-              <td>{product.brand}</td>
-              <td>{product.category}</td>
-              <td className="delete-button">
-                <button onClick={() => onDelete(product.id)}>Delete</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+     <table>
+  <thead>
+    <tr>
+      <th>Thumbnail</th>
+      <th>Title</th>
+      <th>Description</th>
+      <th>Price</th>
+      <th>Rating</th>
+      <th>Stock</th>
+      <th>Brand</th>
+      <th>Category</th>
+      <th>Actions</th>
+    </tr>
+  </thead>
+  <tbody>
+    {products.map((product) => (
+      <tr key={product.id}>
+        <td><img src={product.thumbnail} alt={product.title} /></td>
+        <td>{product.title}</td>
+        <td>{product.description}</td>
+        <td>{product.price}</td>
+        <td>{product.rating}</td>
+        <td>{product.stock}</td>
+        <td>{product.brand}</td>
+        <td>{product.category}</td>
+        <td>
+          <button onClick={() => onDelete(product.id)}>Delete</button>
+        </td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+
     </div>
   );
 };
